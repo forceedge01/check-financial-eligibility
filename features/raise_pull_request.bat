299a2b4a48bf8341d1436fedf14449ac8707e370
Abdul Qureshi <abdul.qureshi@digital.justice.gov.uk>
2022-10-19 10:06:02 +0100

    Moved files

--- features/raise_pull_request.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set CURRENT_BRANCH=%%i
-
-start "" "https://github.com/forceedge01/check-financial-eligibility/compare/main...forceedge01:check-financial-eligibility:%CURRENT_BRANCH%?expand=1"
